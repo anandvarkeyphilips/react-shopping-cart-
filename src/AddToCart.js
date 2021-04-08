dc131c8272c857b4a45b013500479c5e7b0bde4a
Anand Varkey Philips <anandvarkeyphilips@gmail.com>
2021-04-08 11:13:15 +0530

    Day 2 Intro to State and Props

---
+++ src/AddToCart.js
@@ -0,0 +1,61 @@
+
+import React from 'react'
+
+class AddToCart extends React.Component {
+    constructor() {
+        super()
+        this.state = { quantitySelected: 1 }
+    }
+    changeQuantity = (str) => {
+        if (str === "dec") {
+            if (this.state.quantitySelected > 1) {
+                this.setState((prevState) => {
+                    return { quantitySelected: prevState.quantitySelected - 1 }
+                })
+            }
+        }
+        else {
+            if(this.state.quantitySelected < this.props.selectedProd.quantity)
+            {
+                this.setState((prevState)=>{
+                    return {quantitySelected:prevState.quantitySelected+1}
+                })
+            }
+        }
+    }
+    buyEventHandler=()=>{
+        // trigger an event; emmbed the data as part of event;
+        var cartObj={...this.props.selectedProd,quantitySelected:this.state.quantitySelected}
+        this.props.onBuyConfirmation(cartObj)
+    }
+    render() {
+        console.log("Selected product", this.props.selectedProd)
+        var product = this.props.selectedProd;
+        return (
+            <div>
+                <h1> Add To cart Component</h1>
+                <div className="card bg-primary text-white" style={{ width: "18rem" }}>
+                    <img src={product.imageUrl} alt={product.productName}
+                        className="card-img-top" style={{ height: "15rem", width: "17rem" }} />
+                    <div className="card-body">
+                        <h3 className="card-title">{product.productName}</h3>
+                        <p className="card-text">Price :{product.price}</p>
+                        <p className="card-text"> Quantity Available: {product.quantity}</p>
+                        <input type="button" value="-" disabled={this.state.quantitySelected ===1}
+                        className="btn btn-warning" onClick={this.changeQuantity.bind(this, "dec")} />
+                        &nbsp;&nbsp;
+                        {this.state.quantitySelected}
+                        &nbsp;&nbsp;
+                        <input type="button" value="+" disabled={this.state.quantitySelected === this.props.selectedProd.quantity}
+                        className="btn btn-warning" onClick={this.changeQuantity.bind(this, "inc")} />
+                        <br />
+                        <input type="button" value="Buy" onClick={this.buyEventHandler} className="btn btn-warning"/>
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default AddToCart;
--- src/Home.js
+++ src/Home.js
@@ -1,39 +1,70 @@
 import React from "react";
+import AddToCart from './AddToCart';
 import "./Home.css"
+
 // overriding inheritance ; same function with same param sign
-class Home extends React.Component
-{
-    render()
+class Home extends React.Component {
+    constructor()
     {
-        var productsArray=[
+        super();
+        this.ctr=100
+        this.state={showAddToCart:false,selectedProduct:{}}
+        this.detailsEventHandler=this.detailsEventHandler.bind(this);
+        
+    }
+    addToCartEventHandler=(obj)=>{
+        alert("Add to Cart button clicked"+obj.productId);// 100 this== component scope
+        //this.showAddToCart=true;
+        this.setState({showAddToCart:true,selectedProduct:obj},()=>{
+            console.log("Show add to cart",this.state.showAddToCart);//true
+        });
+        // merge the object with the existing state; call the render method 
+       
+
+    }
+    detailsEventHandler()
+    {
+        alert("Details button clicked"+this.ctr);// error; this -- button
+    }
+    onBuyConfirmationEventHandler=(cartObj)=>{
+        console.log("Confirmed Product",cartObj)
+        this.setState({showAddToCart:false})
+    }
+    render() {
+        var productsArray = [
             { productId: 1, productName: "Iphone", price: 68787, imageUrl: "./mobile-1.jpg", quantity: 10, description: "IPhone XR series" },
             { productId: 2, productName: "Samsung", price: 90908, imageUrl: "./mobile-2.jpg", quantity: 15, description: "Samsung Note 9" },
             { productId: 3, productName: "One Plus", price: 36789, imageUrl: "./mobile-3.jpg", quantity: 12, description: "One Plus 6" },
-            { productId: 4, productName: "MI", price: 247678, imageUrl: "./mobile-4.jpg", quantity: 3, description: "MI NOTE 5" }]
-        var trArr=productsArray.map((item)=>{
-                return (
-                    <tr>
-                            <td>
-                                <img src={item.imageUrl} alt={item.productName} />
-                            </td>
-                            <td>
-                            {item.productName}
-                            </td>
-                            <td>
-                            {item.price}
-                            </td>
-                            <td>
-                            {item.quantity}
-                            </td>
-                            <td>
-
-                            </td>
-                        </tr>
-                )
+            { productId: 4, productName: "MI", price: 247678, imageUrl: "./mobile-4.jpg", quantity: 3, description: "MI NOTE 5" }
+        ];
+        var trArr = productsArray.map((item) => {
+            return (
+                <tr key={item.productId}>
+                    <td>
+                        <img src={item.imageUrl} alt={item.productName} />
+                    </td>
+                    <td>
+                        {item.productName}
+                    </td>
+                    <td>
+                        {item.price}
+                    </td>
+                    <td>
+                        {item.quantity}
+                    </td>
+                    <td>
+                        <input type="button" value="Add To Cart" className="btn btn-primary" 
+                        onClick={this.addToCartEventHandler.bind(this,item)} />
+                        &nbsp;&nbsp;
+                        <input type="button" value="Details" className="btn btn-primary" 
+                        onClick={this.detailsEventHandler} />
+                    </td>
+                </tr>
+            )
         })
         return (
             <div>
-                <table className="table bg-warning text">
+                <table className="table bg-warning text-primary">
                     <thead>
                         <tr>
                             <th>Image</th>
@@ -44,9 +75,11 @@ class Home extends React.Component
                         </tr>
                     </thead>
                     <tbody>
-                       {trArr}
+                        {trArr}
                     </tbody>
                 </table>
+                {/* {this.showAddToCart?<AddToCart></AddToCart>:null} */}
+                {this.state.showAddToCart && <AddToCart onBuyConfirmation={this.onBuyConfirmationEventHandler} selectedProd={this.state.selectedProduct}></AddToCart>}
             </div>
         )
     }
